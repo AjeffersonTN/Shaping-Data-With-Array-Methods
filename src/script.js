062fd3a9bedcb70d8f5d0c51701004f76f6d369f
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-05 15:38:42 -0600

    .reduce add to Shaping Data with Array Methods

--- src/script.js
+++ src/script.js
@@ -96,3 +96,21 @@ console.log(foundAgent)
         }
     })
     });
+
+    //using the .reduce
+    // Lightning Exercise 1: Use the reduce method on the following array to determine how much total rain fell last month.
+
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+const totalRainfall = monthlyRainfall.reduce((currentValue, nextValue) => currentValue + nextValue)
+
+console.log(totalRainfall)
+
+
+// Lightning Exercise 2: Use the reduce method on the following array to build a sentence.
+
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce((currentWord, nextWord) => currentWord + " " + nextWord)
+
+console.log(sentence)
